701055fb323e6292bc0e2bb32bcf365529aed652
Sonya <shalamaysonya@gmail.com>
2025-07-20 21:05:55 +0300

    add homework 1 to block 2

---
+++ block 2/Sonya_HW2_Task2/Sonya_HW2_Task2/Sonya_HW2_Task2(UPDATED)/Sonya_HW2_Task2(UPDATED)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sonya_HW2_Task2(UPDATED)
+//
+//  Created by Софія Шаламай on 20.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
